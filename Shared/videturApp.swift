90e3a59f07c0f1ac0c3d21ef3cb3d7ca6634d812
brice <briceguard-git@yahoo.com>
2021-03-27 07:52:13 +0000

    Initial Commit

---
+++ Shared/videturApp.swift
@@ -0,0 +1,17 @@
+//
+//  videturApp.swift
+//  Shared
+//
+//  Created by Brice Rosenzweig on 27/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct videturApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
